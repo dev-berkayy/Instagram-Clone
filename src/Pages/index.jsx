819368ed675658c73c2aca6fd8f849808aabce8f
fatihsen-dev <fatihydrm@outlook.com>
2024-04-04 03:44:53 +0300

    refactor: codes refactored 2

---
+++ src/Pages/index.jsx
@@ -0,0 +1,7 @@
+import Explore from "./Explore";
+import Home from "./Home";
+import Messages from "./Messages";
+import Profile from "./Profile";
+import Reels from "./Reels";
+
+export { Explore, Home, Messages, Profile, Reels };
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,23 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from "./Components/ui/Sidebar/index.jsx";
+import { Explore, Home, Messages, Profile, Reels } from "./Pages";
+import MenuProvider from "./providers/MenuProvider.jsx";
 import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <div className='layout'>
+      <MenuProvider>
+         <BrowserRouter>
+            <Sidebar />
+            <Routes>
+               <Route index element={<Home />} />
+               <Route path='reels' element={<Reels />} />
+               <Route path='messages' element={<Messages />} />
+               <Route path='profile' element={<Profile />} />
+               <Route path='explore' element={<Explore />} />
+            </Routes>
+         </BrowserRouter>
+      </MenuProvider>
+   </div>
+);

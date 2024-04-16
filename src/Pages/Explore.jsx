b9e40eb7b52ccc07b4a0b8a56526f58888ee2563
Berkay-yıldırım <152439115+dev-berkayy@users.noreply.github.com>
2024-04-17 00:32:51 +0300

    Update Explore.jsx

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -3,7 +3,7 @@ import resim1 from "./photos/resim1.jpg"
 import "./pages styles css/explore.css"
 
 
-export default function Explore() {
+export default function Explore() { 
    return (
       <div className='container'>
 
